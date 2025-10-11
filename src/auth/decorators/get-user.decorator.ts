a5b077e96071125a59b91eefa098d7f8a9acacc4
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-10-11 15:11:01 +0100

    updated repo structure

--- src/auth/decorators/get-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-// import { Request } from 'express';
-export const GetUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-
-    return data ? request?.user[data] : request.user;
-  },
-);
