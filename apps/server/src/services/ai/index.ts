88c75a0b34abbfc7b6583f943ff93dd55fc4cbb9
litmus-zhang <abdulsalamlukmon9@gmail.com>
2026-04-23 14:04:43 +0100

    updated restate

--- apps/server/src/services/ai/index.ts
+++ apps/server/src/services/ai/index.ts
@@ -7,7 +7,7 @@ import * as clients from "@restatedev/restate-sdk-clients";
 
 export const restateHandler = restate.createEndpointHandler({
     services: [aiService],
-    identityKeys: config.NODE_ENV === "prod" ? [config.RESTATE_PUBLIC_KEY ?? "publickeyv1_Sample"] : [],
+    identityKeys: config.NODE_ENV === "production" ? [config.RESTATE_PUBLIC_KEY ?? "publickeyv1_Sample"] : [],
     bidirectional: true,
 });
 
