d24f961422b2e4ea4792ba3fdbb169eb17f506d0
litmus-zhang <abdulsalamlukmon9@gmail.com>
2026-04-24 19:19:38 +0100

    refactor: enable secure cookies and cross-subdomain cookie configuration in auth settings

--- apps/server/src/auth/auth.ts
+++ apps/server/src/auth/auth.ts
@@ -46,12 +46,13 @@ export const auth = betterAuth({
   //   useSecureCookies: true,
   //   crossTab: true,
   // },
-  // advanced: {
-  //   crossSubDomainCookies: {
-  //     enabled: true,
-  //     domain: "dynage.technology", // root domain
-  //   },
-  // },
+  advanced: {
+    useSecureCookies: true,
+    crossSubDomainCookies: {
+      enabled: true,
+      domain: "dynage.technology", // root domain
+    },
+  },
   emailVerification: {
     sendVerificationEmail: async ({ user, url, token }) => {
       const modifiedUrl = replaceLocalhostUrl(url, "user")
