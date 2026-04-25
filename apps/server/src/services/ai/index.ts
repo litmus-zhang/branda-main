0f27a93c40e489cedac3f6fbad3258e423926e48
litmus-zhang <abdulsalamlukmon9@gmail.com>
2026-04-25 14:31:18 +0100

    refactor: migrate restate handler to serve method and update sdk import

--- apps/server/src/services/ai/index.ts
+++ apps/server/src/services/ai/index.ts
@@ -1,14 +1,15 @@
 
 import { config } from "../../config.ts";
-import * as restate from "@restatedev/restate-sdk/fetch";
+import * as restate from "@restatedev/restate-sdk";
+// import * as restate from "@restatedev/restate-sdk/fetch";
 import { aiService } from "./restate.ts";
 import * as clients from "@restatedev/restate-sdk-clients";
 
 
-export const restateHandler = restate.createEndpointHandler({
+export const restateHandler = restate.serve({
     services: [aiService],
     identityKeys: config.NODE_ENV === "production" ? [config.RESTATE_PUBLIC_KEY ?? "publickeyv1_Sample"] : [],
-    bidirectional: true,
+    // bidirectional: true,
 });
 
 export const generateBusinessPlan = async (data: {
